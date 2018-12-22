348ff80265666c113c624e3b16e346efeedcda52
vanely <vanelys94@gmail.com>
2018-12-21 22:20:09 -0500

    Python-Challenges

---
+++ KeepHydrated/liters.py
@@ -0,0 +1,11 @@
+import math;
+
+def litres(time):
+
+    return math.floor(time / 2)
+
+#for greater simplicity we could use python floor division
+def liters(t):
+    return t // 2
+
+print(litres(5))
